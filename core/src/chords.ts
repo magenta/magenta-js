18d53104597a2b3654724aa38d9433304398ea41
Ian Simon <iansimon@users.noreply.github.com>
2018-04-06 17:17:09 -0700

    add chord symbol functions (#13)
    * add chord symbol functions
    
    * fix chord functions issues

---
+++ core/src/chords.ts
@@ -0,0 +1,102 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Chord, Note } from 'tonal';
+
+const CHORD_QUALITY_INTERVALS = [
+  ['1P', '3M', '5P'],  // major
+  ['1P', '3m', '5P'],  // minor
+  ['1P', '3M', '5A'],  // augmented
+  ['1P', '3m', '5d'],  // diminished
+];
+
+export enum ChordQuality {
+  Major = 0,
+  Minor = 1,
+  Augmented = 2,
+  Diminished = 3,
+  Other = 4,
+}
+
+export class ChordSymbolException extends Error {
+  constructor(message?: string) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
+
+export class ChordSymbols {
+  /**
+   * Returns an array containing integers (0-11) representing the pitch classes
+   * in a chord.
+   * @param chord A chord symbol string.
+   * @returns An array of integer pitch classes in the chord.
+   * @throws {ChordSymbolException} If the chord cannot be recognized.
+   */
+  public static pitches(chord: string): number[] {
+    if (!Chord.exists(chord)) {
+      throw new ChordSymbolException(
+        'Unrecognized chord symbol: ' + `${chord}`);
+    }
+
+    const notes = Chord.notes(chord);
+    return notes.map(Note.chroma);
+  }
+
+  /**
+   * Returns an integer (0-11) representing the pitch class of the chord root.
+   * @param chord A chord symbol string.
+   * @returns The integer pitch class of the chord root.
+   * @throws {ChordSymbolException} If the chord root cannot be determined.
+   */
+  public static root(chord: string): number {
+    const root = Chord.tokenize(chord)[0];
+    if (!root) {
+      throw new ChordSymbolException(
+        'Chord symbol has unknown root: ' + `${chord}`);
+    }
+
+    return Note.chroma(root);
+  }
+
+  /**
+   * Returns the chord quality (major, minor, augmented, diminished, or other).
+   * @param chord A chord symbol string.
+   * @returns The ChordQuality enum value specifying the quality.
+   * @throws {ChordSymbolException} If the chord cannot be recognized.
+   */
+  public static quality(chord: string): ChordQuality {
+    if (!Chord.exists(chord)) {
+      throw new ChordSymbolException(
+        'Unrecognized chord symbol: ' + `${chord}`);
+    }
+
+    const intervals = Chord.intervals(chord);
+    const qualities = CHORD_QUALITY_INTERVALS.map(
+      cqis => cqis.every(cqi => intervals.includes(cqi)));
+
+    const i = qualities.indexOf(true);
+    const j = qualities.lastIndexOf(true);
+
+    if (i >= 0 && i === j) {
+      return i;
+    }
+    else {
+      return ChordQuality.Other;
+    }
+  }
+}
---
+++ core/src/chords_test.ts
@@ -0,0 +1,84 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as test from 'tape';
+import { ChordQuality, ChordSymbols } from './chords';
+
+test("Test Chord Pitches", (t: test.Test) => {
+  t.deepEqual(
+    ChordSymbols.pitches('Am').sort((x, y) => x - y), [0, 4, 9]);
+  t.deepEqual(
+    ChordSymbols.pitches('D7b9').sort((x, y) => x - y), [0, 2, 3, 6, 9]);
+  t.deepEqual(
+    ChordSymbols.pitches('Fm7b5').sort((x, y) => x - y), [3, 5, 8, 11]);
+  t.deepEqual(
+    ChordSymbols.pitches('CmM7').sort((x, y) => x - y), [0, 3, 7, 11]);
+  t.deepEqual(
+    ChordSymbols.pitches('E##13').sort((x, y) => x - y), [1, 3, 4, 6, 8, 10]);
+  t.deepEqual(
+    ChordSymbols.pitches('G+').sort((x, y) => x - y), [3, 7, 11]);
+  t.end();
+});
+
+test("Test Chord Root", (t: test.Test) => {
+  // Note: not all of these chords can be parsed, but even so the roots can be
+  // correctly identified.
+  t.equal(ChordSymbols.root('Dm9'), 2);
+  t.equal(ChordSymbols.root('E/G#'), 4);
+  t.equal(ChordSymbols.root('Bsus2'), 11);
+  t.equal(ChordSymbols.root('Abmaj7'), 8);
+  t.equal(ChordSymbols.root('D##5(add6)'), 4);
+  t.equal(ChordSymbols.root('F(b7)(#9)(b13)'), 5);
+  t.end();
+});
+
+test("Test Chord Quality", (t: test.Test) => {
+  // Test major chords.
+  t.equal(ChordSymbols.quality('B13'), ChordQuality.Major);
+  t.equal(ChordSymbols.quality('E7#9'), ChordQuality.Major);
+  t.equal(ChordSymbols.quality('FMadd9'), ChordQuality.Major);
+  t.equal(ChordSymbols.quality('C6'), ChordQuality.Major);
+  t.equal(ChordSymbols.quality('Gmaj13'), ChordQuality.Major);
+
+  // Test minor chords.
+  t.equal(ChordSymbols.quality('C#m9'), ChordQuality.Minor);
+  t.equal(ChordSymbols.quality('Gm7'), ChordQuality.Minor);
+  t.equal(ChordSymbols.quality('CbmMaj7'), ChordQuality.Minor);
+  t.equal(ChordSymbols.quality('Am6'), ChordQuality.Minor);
+  t.equal(ChordSymbols.quality('Bbm'), ChordQuality.Minor);
+
+  // Test augmented chords.
+  t.equal(ChordSymbols.quality('D+add#9'), ChordQuality.Augmented);
+  t.equal(ChordSymbols.quality('A+'), ChordQuality.Augmented);
+  t.equal(ChordSymbols.quality('G7#5'), ChordQuality.Augmented);
+  t.equal(ChordSymbols.quality('Faug'), ChordQuality.Augmented);
+
+  // Test diminished chords.
+  t.equal(ChordSymbols.quality('Am7b5'), ChordQuality.Diminished);
+  t.equal(ChordSymbols.quality('Edim7'), ChordQuality.Diminished);
+  t.equal(ChordSymbols.quality('Bbmb5'), ChordQuality.Diminished);
+  t.equal(ChordSymbols.quality('Fo'), ChordQuality.Diminished);
+
+  // Test other chords.
+  t.equal(ChordSymbols.quality('G7no5'), ChordQuality.Other);
+  t.equal(ChordSymbols.quality('Bbsus2'), ChordQuality.Other);
+  t.equal(ChordSymbols.quality('Dsus'), ChordQuality.Other);
+  t.equal(ChordSymbols.quality('Esus24'), ChordQuality.Other);
+  t.equal(ChordSymbols.quality('Em7#5'), ChordQuality.Other);
+
+  t.end();
+});
