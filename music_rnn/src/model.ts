f222cee00ae7b61c40fef292618f070ef39189d7
Tero Parviainen <tero@teropa.info>
2018-04-10 08:32:19 +0300

    Pull out attention for now

--- music_rnn/src/model.ts
+++ music_rnn/src/model.ts
@@ -36,15 +36,6 @@ export class MusicRNN {
   forgetBias: tf.Scalar;
   biasShapes: number[];
 
-  attnInputMatrix?: tf.Tensor2D;
-  attnInputBias?: tf.Tensor1D;
-  attnW?:  tf.Tensor4D;
-  attnV?: tf.Tensor2D;
-  attnMatrix?: tf.Tensor2D;
-  attnBias?: tf.Tensor1D;
-  attnOutputMatrix?: tf.Tensor2D;
-  attnOutputBias?: tf.Tensor1D;
-
   rawVars: {[varName: string]: tf.Tensor};  // Store for disposal.
 
   initialized: boolean;
@@ -84,8 +75,7 @@ export class MusicRNN {
 
     const reader = new magenta.CheckpointLoader(this.checkpointURL);
     const vars = await reader.getAllVariables();
-    const hasAttention = 'rnn/attention_cell_wrapper/kernel' in vars;
-    const rnnPrefix = hasAttention ? 'rnn/attention_cell_wrapper/' : 'rnn/';
+    const rnnPrefix = 'rnn/';
 
     this.forgetBias = tf.scalar(1.0);
 
@@ -108,19 +98,6 @@ export class MusicRNN {
 
     this.lstmFcW = vars['fully_connected/weights'] as tf.Tensor2D;
     this.lstmFcB = vars['fully_connected/biases'] as tf.Tensor1D;
-
-    if (hasAttention) {
-      this.attnInputMatrix = vars[rnnPrefix + 'kernel'] as tf.Tensor2D;
-      this.attnInputBias = vars[rnnPrefix + 'bias'] as tf.Tensor1D;
-      this.attnW = vars[rnnPrefix + 'attention/attn_w'] as tf.Tensor4D;
-      this.attnV = vars[rnnPrefix + 'attention/attn_v'] as tf.Tensor2D;
-      this.attnMatrix = vars[rnnPrefix + 'attention/kernel'] as tf.Tensor2D;
-      this.attnBias = vars[rnnPrefix + 'attention/bias'] as tf.Tensor1D;
-      this.attnOutputMatrix =
-        vars[rnnPrefix + 'attention_output_projection/kernel'] as tf.Tensor2D;
-      this.attnOutputBias =
-        vars[rnnPrefix + 'attention_output_projection/bias'] as tf.Tensor1D;
-    }
 
     this.rawVars = vars;
     this.initialized = true;
@@ -156,9 +133,7 @@ export class MusicRNN {
     const oh = tf.tidy(() => {
       const inputs = this.dataConverter.toTensor(sequence);
       const outputSize:number = inputs.shape[1];
-      const samples = this.attnMatrix ?
-        this.sampleRnnWithAttention(inputs, steps, temperature) :
-        this.sampleRnn(inputs, steps, temperature);
+      const samples = this.sampleRnn(inputs, steps, temperature);
       return tf.stack(samples).as2D(samples.length, outputSize);
     });
 
@@ -196,117 +171,6 @@ export class MusicRNN {
           samples.push(nextInput.as1D().toBool());
       }
       [c, h] = tf.multiRNNCell(this.lstmCells, nextInput, c, h);
-    }
-    return samples;
-  }
-
-  private sampleRnnWithAttention(inputs: tf.Tensor2D,
-                                 steps: number,
-                                 temperature: number) {
-    const length:number = inputs.shape[0];
-    const outputSize:number = inputs.shape[1];
-
-    let c: tf.Tensor2D[] = [];
-    let h: tf.Tensor2D[] = [];
-    for (let i = 0; i < this.biasShapes.length; i++) {
-      c.push(tf.zeros([1, this.biasShapes[i] / 4]));
-      h.push(tf.zeros([1, this.biasShapes[i] / 4]));
-    }
-    const attnSize = this.biasShapes[0] / 4;
-    const attnLength = 32;
-    const numCounterBits = 6;
-
-    let attention: tf.Tensor1D = tf.zeros([attnSize]);
-    let attentionState: tf.Tensor2D = tf.zeros([1, attnSize * attnLength]);
-    let lastOutput: tf.Tensor2D;
-
-    // Initialize with input.
-    const samples: tf.Tensor1D[] = [];
-    for (let i = 0; i < length + steps; i++) {
-      let nextInput: tf.Tensor2D;
-      if (i < length) {
-        nextInput = inputs.slice([i, 0], [1, outputSize]).as2D(1, outputSize);
-      } else {
-        const logits = lastOutput.matMul(this.lstmFcW).add(this.lstmFcB);
-        const sampledOutput = (
-          temperature ?
-          tf.multinomial(logits.div(tf.scalar(temperature)), 1).as1D():
-          logits.argMax(1).as1D());
-        nextInput = tf.oneHot(sampledOutput, outputSize).toFloat();
-        // Save samples as bool to reduce data sync time.
-        samples.push(nextInput.as1D().toBool());
-      }
-
-      const binaryCounts = tf.buffer([1, numCounterBits]);
-      for (let bit = 0; bit < numCounterBits; bit++) {
-        const counterValue = Math.floor((i + 1) / 2 ** bit) % 2 ? 1.0 : -1.0;
-        binaryCounts.set(counterValue, 0, bit);
-      }
-      nextInput = nextInput.concat(binaryCounts.toTensor(), 1) as tf.Tensor2D;
-
-      const nextAttnInput = tf.concat([nextInput, attention.as2D(1, -1)], 1);
-      const nextRnnInput: tf.Tensor2D = tf.add(
-        tf.matMul(nextAttnInput, this.attnInputMatrix),
-        this.attnInputBias.as2D(1, -1)
-      );
-      [c, h] = tf.multiRNNCell(this.lstmCells, nextRnnInput, c, h);
-
-      const attnHidden: tf.Tensor4D = tf.reshape(
-        attentionState,
-        [-1, attnLength, 1, attnSize]
-      );
-      const attnHiddenFeatures = tf.conv2d(
-        attnHidden,
-        this.attnW,
-        [1, 1],
-        'same'
-      );
-      const attnQueryParts = [];
-      for (let q = 0 ; q < c.length ; q++) {
-        attnQueryParts.push(c[q]);
-        attnQueryParts.push(h[q]);
-      }
-      const attnQuery = tf.concat(attnQueryParts, 1);
-      const attnY = tf
-        .matMul(attnQuery, this.attnMatrix)
-        .reshape([-1, 1, 1, attnSize]);
-      const attnS = tf.sum(
-        tf.mul(this.attnV, tf.tanh(tf.add(attnHiddenFeatures, attnY))),
-        [2, 3]
-      );
-      const attnA = tf.softmax(attnS);
-      const attnD = tf.sum(
-        tf.mul(tf.reshape(attnA, [-1, attnLength, 1, 1]), attnHidden),
-        [1, 2]
-      );
-      const newAttns: tf.Tensor2D = attnD.reshape([-1, attnSize]);
-
-      const attnStates = attentionState.reshape([
-        -1,
-        attnLength,
-        attnSize
-      ]);
-      const newAttnStates = tf.slice(
-        attnStates,
-        [0, 1, 0],
-        [attnStates.shape[0], attnStates.shape[1] - 1, attnStates.shape[2]]
-      );
-
-      lastOutput = tf.add(
-        tf.matMul(tf.concat([h[2], newAttns], 1), this.attnOutputMatrix),
-        this.attnOutputBias
-      );
-
-      attention = newAttns.flatten();
-      attentionState = tf
-        .concat(
-          [
-            newAttnStates,
-            lastOutput.as3D(lastOutput.shape[0], 1, lastOutput.shape[1])
-          ],
-          1
-        )
-        .reshape([-1, attnLength * attnSize]);
     }
     return samples;
   }
