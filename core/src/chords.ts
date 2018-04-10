e4423fe21bbaa5fa5bcf2db4a8e917415124f8c6
Ian Simon <iansimon@users.noreply.github.com>
2018-04-10 09:00:00 -0700

    add chord encoders that map chord symbol to 1D tensor (#16)
    * add chord encoders that map chord symbol to 1D tensor
    
    * minor fixes to chord exceptions

--- core/src/chords.ts
+++ core/src/chords.ts
@@ -15,7 +15,9 @@
  * limitations under the License.
  */
 
+import * as tf from '@tensorflow/tfjs';
 import {Chord, Note} from 'tonal';
+import * as constants from './constants';
 
 const CHORD_QUALITY_INTERVALS = [
   ['1P', '3M', '5P'],  // major
@@ -39,6 +41,13 @@ export class ChordSymbolException extends Error {
   }
 }
 
+export class ChordEncodingException extends Error {
+  constructor(message?: string) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
+
 export class ChordSymbols {
   /**
    * Returns an array containing integers (0-11) representing the pitch classes
@@ -49,9 +58,7 @@ export class ChordSymbols {
    */
   public static pitches(chord: string): number[] {
     if (!Chord.exists(chord)) {
-      throw new ChordSymbolException(
-          'Unrecognized chord symbol: ' +
-          `${chord}`);
+      throw new ChordSymbolException(`Unrecognized chord symbol: ${chord}`);
     }
 
     const notes = Chord.notes(chord);
@@ -67,9 +74,7 @@ export class ChordSymbols {
   public static root(chord: string): number {
     const root = Chord.tokenize(chord)[0];
     if (!root) {
-      throw new ChordSymbolException(
-          'Chord symbol has unknown root: ' +
-          `${chord}`);
+      throw new ChordSymbolException(`Chord symbol has unknown root: ${chord}`);
     }
 
     return Note.chroma(root);
@@ -83,9 +88,7 @@ export class ChordSymbols {
    */
   public static quality(chord: string): ChordQuality {
     if (!Chord.exists(chord)) {
-      throw new ChordSymbolException(
-          'Unrecognized chord symbol: ' +
-          `${chord}`);
+      throw new ChordSymbolException(`Unrecognized chord symbol: ${chord}`);
     }
 
     const intervals = Chord.intervals(chord);
@@ -100,5 +103,100 @@ export class ChordSymbols {
     } else {
       return ChordQuality.Other;
     }
+  }
+}
+
+/**
+ * Abstract ChordEncoder class for converting chord symbols to tensors.
+ */
+export abstract class ChordEncoder {
+  abstract depth: number;
+  abstract encode(chord: string): tf.Tensor1D;
+}
+
+/**
+ * ChordEncoder that outputs a one-hot encoding over major and minor triads.
+ */
+export class MajorMinorChordEncoder extends ChordEncoder {
+  depth = 1 + 2 * constants.NUM_PITCH_CLASSES;
+
+  private index(chord: string) {
+    if (chord === constants.NO_CHORD) {
+      return 0;
+    }
+
+    const root = ChordSymbols.root(chord);
+    const quality = ChordSymbols.quality(chord);
+    const index = 1 + quality * constants.NUM_PITCH_CLASSES + root;
+
+    if (index >= this.depth) {
+      throw new ChordEncodingException(
+          `Chord is neither major nor minor: ${chord}`);
+    }
+
+    return index;
+  }
+
+  encode(chord: string) {
+    return tf.oneHot(tf.tensor1d([this.index(chord)]), this.depth).as1D();
+  }
+}
+
+/**
+ * ChordEncoder that outputs a one-hot encoding over major, minor, augmented,
+ * and diminished triads.
+ */
+export class TriadChordEncoder extends ChordEncoder {
+  depth = 1 + 4 * constants.NUM_PITCH_CLASSES;
+
+  private index(chord: string) {
+    if (chord === constants.NO_CHORD) {
+      return 0;
+    }
+
+    const root = ChordSymbols.root(chord);
+    const quality = ChordSymbols.quality(chord);
+    const index = 1 + quality * constants.NUM_PITCH_CLASSES + root;
+
+    if (index >= this.depth) {
+      throw new ChordEncodingException(
+          `Chord is not a standard triad: ${chord}`);
+    }
+
+    return index;
+  }
+
+  encode(chord: string) {
+    return tf.oneHot(tf.tensor1d([this.index(chord)]), this.depth).as1D();
+  }
+}
+
+/**
+ * ChordEncoder that outputs (concatenated) a one-hot encoding over chord root
+ * pitch class, a binary vector indicating the pitch classes contained in the
+ * chord, and a one-hot encoding over chord bass pitch class.
+ */
+export class PitchChordEncoder extends ChordEncoder {
+  depth = 1 + 3 * constants.NUM_PITCH_CLASSES;
+
+  encode(chord: string) {
+    if (chord === constants.NO_CHORD) {
+      return tf.oneHot(tf.tensor1d([0]), this.depth).as1D();
+    }
+
+    const root = ChordSymbols.root(chord);
+    const rootEncoding =
+        tf.oneHot(tf.tensor1d([root]), constants.NUM_PITCH_CLASSES).as1D();
+
+    const pitchBuffer = tf.buffer([constants.NUM_PITCH_CLASSES]);
+    ChordSymbols.pitches(chord).forEach(pitch => pitchBuffer.set(1.0, pitch));
+    const pitchEncoding = pitchBuffer.toTensor().as1D();
+
+    // Since tonal doesn't understand chords with slash notation to specify
+    // bass, just use the root for now.
+    const bassEncoding = rootEncoding;
+
+    return tf.concat1d(
+        [tf.tensor1d([0.0]), rootEncoding, pitchEncoding, bassEncoding]);
   }
 }
