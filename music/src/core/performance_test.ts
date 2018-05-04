d423a9b6632c86009cb32a630a997021a7e3a5d4
Ian Simon <iansimon@users.noreply.github.com>
2018-05-04 15:20:37 -0700

    add multitrack data converter (#73)
    * add getNumSteps and setNumSteps to Performance
    
    * add multitrack data converter
    
    * make end token explicit in multitrack converter

--- music/src/core/performance_test.ts
+++ music/src/core/performance_test.ts
@@ -21,11 +21,13 @@ import {Performance, PerformanceEvent} from './performance';
 
 test('From NoteSequence', (t: test.Test) => {
   const noteSequence = NoteSequence.create({
+    quantizationInfo: {stepsPerSecond: 10},
     notes: [
       {pitch: 60, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 40},
       {pitch: 64, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 30},
       {pitch: 67, velocity: 127, quantizedStartStep: 10, quantizedEndStep: 20},
-    ]
+    ],
+    totalQuantizedSteps: 40
   });
   const performance = Performance.fromNoteSequence(noteSequence, 10, 0);
   const expectedEvents: PerformanceEvent[] = [
@@ -46,11 +48,13 @@ test('From NoteSequence', (t: test.Test) => {
 
 test('From NoteSequence With Velocity', (t: test.Test) => {
   const noteSequence = NoteSequence.create({
+    quantizationInfo: {stepsPerSecond: 10},
     notes: [
       {pitch: 60, velocity: 100, quantizedStartStep: 0, quantizedEndStep: 40},
       {pitch: 64, velocity: 100, quantizedStartStep: 0, quantizedEndStep: 30},
       {pitch: 67, velocity: 127, quantizedStartStep: 10, quantizedEndStep: 20},
-    ]
+    ],
+    totalQuantizedSteps: 40
   });
   const performance = Performance.fromNoteSequence(noteSequence, 10, 127);
   const expectedEvents: PerformanceEvent[] = [
@@ -73,11 +77,13 @@ test('From NoteSequence With Velocity', (t: test.Test) => {
 
 test('From NoteSequence With Quantized Velocity', (t: test.Test) => {
   const noteSequence = NoteSequence.create({
+    quantizationInfo: {stepsPerSecond: 10},
     notes: [
       {pitch: 60, velocity: 100, quantizedStartStep: 0, quantizedEndStep: 40},
       {pitch: 64, velocity: 100, quantizedStartStep: 0, quantizedEndStep: 30},
       {pitch: 67, velocity: 127, quantizedStartStep: 10, quantizedEndStep: 20},
-    ]
+    ],
+    totalQuantizedSteps: 40
   });
   const performance = Performance.fromNoteSequence(noteSequence, 10, 16);
   const expectedEvents: PerformanceEvent[] = [
@@ -100,6 +106,7 @@ test('From NoteSequence With Quantized Velocity', (t: test.Test) => {
 
 test('From NoteSequence (Program and Drum Status)', (t: test.Test) => {
   const noteSequence = NoteSequence.create({
+    quantizationInfo: {stepsPerSecond: 10},
     notes: [
       {pitch: 60, quantizedEndStep: 40, instrument: 0, program: 1},
       {pitch: 64, quantizedEndStep: 30, instrument: 0, program: 1},
@@ -107,7 +114,8 @@ test('From NoteSequence (Program and Drum Status)', (t: test.Test) => {
       {pitch: 36, quantizedEndStep: 40, instrument: 1, program: 2},
       {pitch: 48, quantizedEndStep: 40, instrument: 1, program: 2},
       {pitch: 57, quantizedEndStep: 1, instrument: 2, program: 0, isDrum: true},
-    ]
+    ],
+    totalQuantizedSteps: 40
   });
   const performance = Performance.fromNoteSequence(noteSequence, 10, 0);
   const performance0 = Performance.fromNoteSequence(noteSequence, 10, 0, 0);
@@ -121,6 +129,64 @@ test('From NoteSequence (Program and Drum Status)', (t: test.Test) => {
   t.equal(performance1.isDrum, false);
   t.equal(performance2.program, undefined);
   t.equal(performance2.isDrum, true);
+  t.end();
+});
+
+test('Add Steps', (t: test.Test) => {
+  const performance = new Performance([], 10, 0);
+  performance.setNumSteps(5);
+  t.equal(performance.getNumSteps(), 5);
+  t.deepEqual(performance.events, [{type: 'time-shift', steps: 5}]);
+
+  performance.setNumSteps(15);
+  t.equal(performance.getNumSteps(), 15);
+  t.deepEqual(
+      performance.events,
+      [{type: 'time-shift', steps: 10}, {type: 'time-shift', steps: 5}]);
+
+  performance.setNumSteps(20);
+  t.equal(performance.getNumSteps(), 20);
+  t.deepEqual(
+      performance.events,
+      [{type: 'time-shift', steps: 10}, {type: 'time-shift', steps: 10}]);
+
+  t.end();
+});
+
+test('Remove Steps', (t: test.Test) => {
+  const events: PerformanceEvent[] = [
+    {type: 'note-on', pitch: 60},
+    {type: 'time-shift', steps: 10},
+    {type: 'note-off', pitch: 60},
+    {type: 'note-on', pitch: 64},
+    {type: 'time-shift', steps: 10},
+    {type: 'note-off', pitch: 64},
+    {type: 'note-on', pitch: 67},
+    {type: 'time-shift', steps: 10},
+    {type: 'note-off', pitch: 67},
+  ];
+  const performance = new Performance(events, 10, 0);
+
+  performance.setNumSteps(20);
+  const expectedEvents: PerformanceEvent[] = [
+    {type: 'note-on', pitch: 60},
+    {type: 'time-shift', steps: 10},
+    {type: 'note-off', pitch: 60},
+    {type: 'note-on', pitch: 64},
+    {type: 'time-shift', steps: 10},
+    {type: 'note-off', pitch: 64},
+    {type: 'note-on', pitch: 67},
+
+  ];
+  t.equal(performance.getNumSteps(), 20);
+  t.deepEqual(performance.events, expectedEvents);
+
+  performance.setNumSteps(5);
+  t.equal(performance.getNumSteps(), 5);
+  t.deepEqual(
+      performance.events,
+      [{type: 'note-on', pitch: 60}, {type: 'time-shift', steps: 5}]);
+
   t.end();
 });
 
@@ -141,10 +207,11 @@ test('To NoteSequence', (t: test.Test) => {
   const noteSequence = performance.toNoteSequence();
   const expectedNoteSequence = NoteSequence.create({
     notes: [
-      {pitch: 67, velocity: 127, quantizedStartStep: 10, quantizedEndStep: 20},
-      {pitch: 64, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 30},
-      {pitch: 60, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 40},
-    ]
+      {pitch: 67, quantizedStartStep: 10, quantizedEndStep: 20},
+      {pitch: 64, quantizedStartStep: 0, quantizedEndStep: 30},
+      {pitch: 60, quantizedStartStep: 0, quantizedEndStep: 40},
+    ],
+    totalQuantizedSteps: 40
   });
   t.deepEqual(noteSequence, expectedNoteSequence);
   t.end();
@@ -173,7 +240,8 @@ test('To NoteSequence With Velocity', (t: test.Test) => {
       {pitch: 67, velocity: 127, quantizedStartStep: 10, quantizedEndStep: 20},
       {pitch: 64, velocity: 115, quantizedStartStep: 0, quantizedEndStep: 30},
       {pitch: 60, velocity: 100, quantizedStartStep: 0, quantizedEndStep: 40},
-    ]
+    ],
+    totalQuantizedSteps: 40
   });
   t.deepEqual(noteSequence, expectedNoteSequence);
   t.end();
@@ -201,7 +269,8 @@ test('To NoteSequence With Quantized Velocity', (t: test.Test) => {
       {pitch: 67, velocity: 121, quantizedStartStep: 10, quantizedEndStep: 20},
       {pitch: 64, velocity: 97, quantizedStartStep: 0, quantizedEndStep: 30},
       {pitch: 60, velocity: 97, quantizedStartStep: 0, quantizedEndStep: 40},
-    ]
+    ],
+    totalQuantizedSteps: 40
   });
   t.deepEqual(noteSequence, expectedNoteSequence);
   t.end();
@@ -220,9 +289,10 @@ test('To NoteSequence With Unmatched Note-Offs', (t: test.Test) => {
   const noteSequence = performance.toNoteSequence();
   const expectedNoteSequence = NoteSequence.create({
     notes: [
-      {pitch: 60, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 5},
-      {pitch: 64, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 5},
-    ]
+      {pitch: 60, quantizedStartStep: 0, quantizedEndStep: 5},
+      {pitch: 64, quantizedStartStep: 0, quantizedEndStep: 5},
+    ],
+    totalQuantizedSteps: 5
   });
   t.deepEqual(noteSequence, expectedNoteSequence);
   t.end();
@@ -238,9 +308,10 @@ test('To NoteSequence With Unmatched Note-Ons', (t: test.Test) => {
   const noteSequence = performance.toNoteSequence();
   const expectedNoteSequence = NoteSequence.create({
     notes: [
-      {pitch: 60, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 10},
-      {pitch: 64, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 10},
-    ]
+      {pitch: 60, quantizedStartStep: 0, quantizedEndStep: 10},
+      {pitch: 64, quantizedStartStep: 0, quantizedEndStep: 10},
+    ],
+    totalQuantizedSteps: 10
   });
   t.deepEqual(noteSequence, expectedNoteSequence);
   t.end();
@@ -258,10 +329,11 @@ test('To NoteSequence With Repeated Notes', (t: test.Test) => {
   const noteSequence = performance.toNoteSequence();
   const expectedNoteSequence = NoteSequence.create({
     notes: [
-      {pitch: 60, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 20},
-      {pitch: 60, velocity: 127, quantizedStartStep: 10, quantizedEndStep: 20},
-      {pitch: 64, velocity: 127, quantizedStartStep: 0, quantizedEndStep: 20},
-    ]
+      {pitch: 60, quantizedStartStep: 0, quantizedEndStep: 20},
+      {pitch: 60, quantizedStartStep: 10, quantizedEndStep: 20},
+      {pitch: 64, quantizedStartStep: 0, quantizedEndStep: 20},
+    ],
+    totalQuantizedSteps: 20
   });
   t.deepEqual(noteSequence, expectedNoteSequence);
   t.end();
