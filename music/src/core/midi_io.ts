d8e9511d1eafd3c08cbce046bca8e13456857ca4
Adam Roberts <adarob@google.com>
2018-05-20 18:29:06 -0700

    Fill in missing values with defaults when outputting MIDI.

--- music/src/core/midi_io.ts
+++ music/src/core/midi_io.ts
@@ -22,6 +22,7 @@
  * Imports
  */
 import * as midiconvert from 'midiconvert';
+import {isNullOrUndefined} from 'util';
 
 import {INoteSequence, NoteSequence} from '../protobuf/index';
 
@@ -105,58 +106,80 @@ export function midiToSequenceProto(midi: string): NoteSequence {
 }
 
 export function sequenceProtoToMidi(ns: INoteSequence) {
-  if (!ns.tempos || ns.tempos.length !== 1 || ns.tempos[0].time !== 0) {
+  if (!ns.tempos || ns.tempos.length === 0) {
+    ns.tempos = [{time: 0, qpm: constants.DEFAULT_QUARTERS_PER_MINUTE}];
+  }
+  if (!ns.timeSignatures || ns.timeSignatures.length === 0) {
+    ns.timeSignatures = [{time: 0, numerator: 4, denominator: 4}];
+  }
+
+  if (ns.tempos.length !== 1 || ns.tempos[0].time !== 0) {
     throw new MidiConversionError(
         'NoteSequence must have exactly 1 tempo at time 0');
   }
-  if (!ns.timeSignatures || ns.timeSignatures.length !== 1 ||
-      ns.timeSignatures[0].time !== 0) {
+  if (ns.timeSignatures.length !== 1 || ns.timeSignatures[0].time !== 0) {
     throw new MidiConversionError(
         'NoteSequence must have exactly 1 time signature at time 0');
   }
   const json = {
     header: {
       bpm: ns.tempos[0].qpm,
-      PPQ: ns.ticksPerQuarter,
+      PPQ: ns.ticksPerQuarter ? ns.ticksPerQuarter :
+                                constants.DEFAULT_TICKS_PER_QUARTER,
       timeSignature:
           [ns.timeSignatures[0].numerator, ns.timeSignatures[0].denominator]
     },
     tracks: [] as Array<{}>
   };
-  const tracks: {[instrument: number]: NoteSequence.INote[]} = {};
+
+  const tracks = new Map<number, Array<NoteSequence.INote>>();
   for (const note of ns.notes) {
-    const track = note.instrument;
-    if (!(track in tracks)) {
-      tracks[track] = [];
+    const instrument = note.instrument ? note.instrument : 0;
+    if (!tracks.has(instrument)) {
+      tracks.set(instrument, []);
     }
-    tracks[track].push(note);
+    tracks.get(instrument).push(note);
   }
-  const instruments = Object.keys(tracks).map(x => parseInt(x, 10)).sort();
+  const instruments = Array.from(tracks.keys()).sort();
   for (let i = 0; i < instruments.length; i++) {
     if (i !== instruments[i]) {
       throw new MidiConversionError(
           'Instrument list must be continuous and start at 0');
     }
 
+    const notes = tracks.get(i);
     const track = {
       id: i,
       notes: [] as Array<{}>,
-      isPercussion: tracks[i][0].isDrum,
+      isPercussion: isNullOrUndefined(notes[0].isDrum) ? false :
+                                                         notes[0].isDrum,
       channelNumber: i,
-      instrumentNumber: tracks[i][0].program
+      instrumentNumber: isNullOrUndefined(notes[0].program) ?
+          constants.DEFAULT_PROGRAM :
+          notes[0].program
     };
 
-    for (const note of tracks[i]) {
-      track.notes.push({
-        midi: note.pitch,
-        time: note.startTime,
-        duration: note.endTime - note.startTime,
-        velocity: (note.velocity as number + 1) / constants.MIDI_VELOCITIES
-      });
-    }
+    track.notes = notes.map(note => {
+      const velocity = isNullOrUndefined(note.velocity) ?
+          constants.DEFAULT_VELOCITY :
+          note.velocity;
+      return {
+        midi: note.pitch, time: note.startTime,
+            duration: note.endTime - note.startTime,
+            velocity: (velocity as number + 1) / constants.MIDI_VELOCITIES
+      }
+    });
 
     json['tracks'].push(track);
   }
 
   return midiconvert.fromJSON(json).encode();
+}
+
+export function downloadMidi(midi: string) {
+  const buffer = new Uint8Array(midi.length)
+  for (let i = 0; i < midi.length; i++) {
+    buffer[i] = midi.charCodeAt(i)
+  }
+  return buffer
 }
--- music/src/core/midi_io_test.ts
+++ music/src/core/midi_io_test.ts
@@ -20,138 +20,166 @@ import * as test from 'tape';
 
 import {NoteSequence} from '../protobuf/index';
 
+import * as constants from './constants';
 import * as midi_io from './midi_io';
+import * as sequences from './sequences';
+
+const simpleNs = NoteSequence.create({
+  ticksPerQuarter: 220,
+  totalTime: 1.5,
+  timeSignatures: [{time: 0, numerator: 4, denominator: 4}],
+  tempos: [{time: 0, qpm: 120}],
+  sourceInfo: {
+    encodingType: NoteSequence.SourceInfo.EncodingType.MIDI,
+    parser: NoteSequence.SourceInfo.Parser.TONEJS_MIDI_CONVERT
+  },
+  notes: [
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 0,
+      endTime: 0.125,
+      pitch: 60,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 0.125,
+      endTime: 0.25,
+      pitch: 62,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 0.25,
+      endTime: 0.375,
+      pitch: 64,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 0.375,
+      endTime: 0.5,
+      pitch: 66,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 0.5,
+      endTime: 0.625,
+      pitch: 68,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 0.625,
+      endTime: 0.75,
+      pitch: 70,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 0.75,
+      endTime: 0.875,
+      pitch: 72,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 0.875,
+      endTime: 1,
+      pitch: 70,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 1,
+      endTime: 1.125,
+      pitch: 68,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 1.125,
+      endTime: 1.25,
+      pitch: 66,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 1.25,
+      endTime: 1.375,
+      pitch: 64,
+      velocity: 100,
+      isDrum: false
+    },
+    {
+      instrument: 0,
+      program: 0,
+      startTime: 1.375,
+      endTime: 1.5,
+      pitch: 62,
+      velocity: 100,
+      isDrum: false
+    }
+  ]
+});
 
 test('Parse Simple MIDI', (t: test.Test) => {
   const midi = fs.readFileSync('../testdata/melody.mid', 'binary');
   const ns = midi_io.midiToSequenceProto(midi);
 
-  const expectedNs = NoteSequence.create({
-    ticksPerQuarter: 220,
-    totalTime: 1.5,
-    timeSignatures: [NoteSequence.TimeSignature.create(
-        {time: 0, numerator: 4, denominator: 4})],
-    tempos: [NoteSequence.Tempo.create({time: 0, qpm: 120})],
-    sourceInfo: NoteSequence.SourceInfo.create({
-      encodingType: NoteSequence.SourceInfo.EncodingType.MIDI,
-      parser: NoteSequence.SourceInfo.Parser.TONEJS_MIDI_CONVERT
-    }),
-    notes: [
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 0,
-        endTime: 0.125,
-        pitch: 60,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 0.125,
-        endTime: 0.25,
-        pitch: 62,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 0.25,
-        endTime: 0.375,
-        pitch: 64,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 0.375,
-        endTime: 0.5,
-        pitch: 66,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 0.5,
-        endTime: 0.625,
-        pitch: 68,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 0.625,
-        endTime: 0.75,
-        pitch: 70,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 0.75,
-        endTime: 0.875,
-        pitch: 72,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 0.875,
-        endTime: 1,
-        pitch: 70,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 1,
-        endTime: 1.125,
-        pitch: 68,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 1.125,
-        endTime: 1.25,
-        pitch: 66,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 1.25,
-        endTime: 1.375,
-        pitch: 64,
-        velocity: 100,
-        isDrum: false
-      }),
-      NoteSequence.Note.create({
-        instrument: 0,
-        program: 0,
-        startTime: 1.375,
-        endTime: 1.5,
-        pitch: 62,
-        velocity: 100,
-        isDrum: false
-      })
-    ]
-  });
-
-  t.deepEqual(ns, expectedNs);
+  t.deepEqual(ns, simpleNs);
 
   const nsRoundTrip =
       midi_io.midiToSequenceProto(midi_io.sequenceProtoToMidi(ns));
+
+  t.deepEqual(nsRoundTrip, simpleNs);
+
+  t.end();
+});
+
+test('Write MIDI Using Defaults', (t: test.Test) => {
+  const strippedNs = sequences.clone(simpleNs);
+  strippedNs.tempos = undefined;
+  strippedNs.timeSignatures = undefined;
+  strippedNs.ticksPerQuarter = undefined;
+  strippedNs.notes.forEach(n => {
+    n.velocity = undefined;
+    n.isDrum = undefined;
+    n.instrument = undefined;
+    n.program = undefined;
+  })
+
+
+  const expectedNs = sequences.clone(simpleNs);
+  expectedNs.notes.forEach(n => {
+    n.velocity = constants.DEFAULT_VELOCITY;
+    n.program = constants.DEFAULT_PROGRAM;
+  })
+
+  const nsRoundTrip =
+      midi_io.midiToSequenceProto(midi_io.sequenceProtoToMidi(strippedNs));
 
   t.deepEqual(nsRoundTrip, expectedNs);
 
