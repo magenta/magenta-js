a47c70ec60b2b190ecf3c70e8b72c14f75e6cdaa
Adam Roberts <adarob@google.com>
2018-08-22 13:12:13 -0700

    more updates

--- music/src/transcription/model.ts
+++ music/src/transcription/model.ts
@@ -22,7 +22,7 @@
  */
 import * as tf from '@tensorflow/tfjs';
 
-import {INoteSequence, NoteSequence} from '../protobuf/index';
+import {INoteSequence, NoteSequence} from '../protobuf';
 
 const MIN_MIDI_PITCH = 21;
 const MAX_MIDI_PITCH = 108;
@@ -33,23 +33,103 @@ const SPEC_HOP_LENGTH = 512;
 const FRAME_LENGTH_SECONDS = SPEC_HOP_LENGTH / SAMPLE_RATE;
 
 export class OnsetsAndFrames {
+  private checkpointURL: string;
+  private initialized: boolean;
+
   private onsetsModel: tf.Sequential;
-  private velocitiesModel: tf.Sequential;
+  private velocityModel: tf.Sequential;
   private activationModel: tf.Sequential;
   private frameModel: tf.Model;
 
-  private build() {
-    const onsetsModel = this.getAcousticModel();
-    onsetsModel.add(
-        tf.layers.dense({units: MIDI_PITCHES, activation: 'sigmoid'}));
+  constructor(checkpointURL: string) {
+    this.checkpointURL = checkpointURL;
+  }
 
-    const velocitiesModel = this.getAcousticModel();
-    velocitiesModel.add(
-        tf.layers.dense({units: MIDI_PITCHES, activation: undefined}));
+  async initialize() {
+    // this.dispose();
 
-    const activationModel = this.getAcousticModel();
-    activationModel.add(
-        tf.layers.dense({units: MIDI_PITCHES, activation: 'sigmoid'}));
+    const rawVars =
+        await fetch(`${this.checkpointURL}/weights_manifest.json`)
+            .then((response) => response.json())
+            .then(
+                (manifest: tf.io.WeightsManifestConfig) =>
+                    tf.io.loadWeights(manifest, this.checkpointURL));
+    const vars = new Map<string, tf.Tensor>();
+    Object.keys(rawVars).map(name => vars.set(name, rawVars[name]));
+
+    this.build(vars);
+    this.initialized = true
+  }
+
+  /**
+   * Returns true iff model is intialized.
+   */
+  isInitialized() {
+    return this.initialized;
+  }
+
+  async transcribeFromMelSpec(melSpec: number[][]) {
+    if (!this.isInitialized) {
+      this.initialize();
+    }
+    // Add batch dim.
+    const melSpecBatch = tf.tensor3d([melSpec]);
+
+    const onsetProbs = this.onsetsModel.predict(melSpecBatch) as tf.Tensor3D;
+    const velocities = this.velocityModel.predict(melSpecBatch) as tf.Tensor3D;
+    const activationProbs =
+        this.activationModel.predict(melSpecBatch) as tf.Tensor3D;
+    const frameProbs =
+        this.frameModel.predict([onsetProbs, activationProbs]) as tf.Tensor3D;
+
+    // Squeeze batch dims.
+    return this.pianorollToNoteSequence(
+        frameProbs.squeeze(), onsetProbs.squeeze(), velocities.squeeze());
+  }
+
+  private build(vars: Map<string, tf.Tensor>) {
+    function convWeights(prefix: string) {
+      return [
+        vars.get(`{prefix}/conv1/weights`),
+        vars.get(`{prefix}/conv1/BatchNorm/beta`),
+        vars.get(`{prefix}/conv1/BatchNorm/moving_mean`),
+        vars.get(`{prefix}/conv1/BatchNorm/moving_variance`),
+        vars.get(`{prefix}/conv2/weights`),
+        vars.get(`{prefix}/conv2/BatchNorm/beta`),
+        vars.get(`{prefix}/conv2/BatchNorm/moving_mean`),
+        vars.get(`{prefix}/conv2/BatchNorm/moving_variance`),
+        vars.get(`{prefix}/conv3/weights`),
+        vars.get(`{prefix}/conv3/BatchNorm/beta`),
+        vars.get(`{prefix}/conv3/BatchNorm/moving_mean`),
+        vars.get(`{prefix}/conv3/BatchNorm/moving_variance`),
+        vars.get(`{prefix}/fc5/weights`),
+        vars.get(`{prefix}/fc5/biases`),
+        vars.get(`{prefix}/bidirectional_rnn/bw/lstm_cell/kernel`),
+        vars.get(`{prefix}/bidirectional_rnn/bw/lstm_cell/bias`),
+      ];
+    }
+    function lstmWeights(prefix: string) {
+      return [
+        vars.get(`{prefix}/bidirectional_rnn/bw/lstm_cell/kernel`),
+        vars.get(`{prefix}/bidirectional_rnn/bw/lstm_cell/bias`)
+      ];
+    }
+    function denseWeights(prefix: string) {
+      return [vars.get(`{prefix}/weights`), vars.get(`{prefix}/biases`)];
+    }
+
+    const onsetsModel = this.getAcousticModel('sigmoid');
+    onsetsModel.setWeights(convWeights('onsets').concat(
+        lstmWeights('onsets'), denseWeights('onsets/onset_probs')));
+
+    const velocityModel = this.getAcousticModel(undefined);
+    velocityModel.setWeights(
+        convWeights('velocity')
+            .concat(denseWeights('velocity/onset_velocities')));
+
+    const activationModel = this.getAcousticModel('sigmoid');
+    activationModel.setWeights(
+        convWeights('frame').concat(denseWeights('frame/activation_probs')));
 
     const onsetsInput = tf.input({shape: onsetsModel.outputShape as number[]})
     const activationInput =
@@ -59,26 +139,18 @@ export class OnsetsAndFrames {
     net = tf.layers.bidirectional({layer: rnn, mergeMode: 'concat'}).apply(net);
     net = tf.layers.dense({units: MIDI_PITCHES, activation: 'sigmoid'})
               .apply(net) as tf.SymbolicTensor;
-
-    this.frameModel =
+    const frameModel =
         tf.model({inputs: [onsetsInput, activationInput], outputs: net});
+    frameModel.setWeights(
+        lstmWeights('frame').concat(denseWeights('frame/frame_probs')));
+
+    this.frameModel = frameModel;
     this.onsetsModel = onsetsModel;
-    this.velocitiesModel = velocitiesModel;
+    this.velocityModel = velocityModel;
     this.activationModel = activationModel;
   }
 
-  public transcribe(melSpec: tf.Tensor2D) {
-    const melSpecBatch = melSpec.expandDims(0);
-    const onsetsProbs = this.onsetsModel.predict(melSpecBatch) as tf.Tensor3D;
-    const velocities =
-        this.velocitiesModel.predict(melSpecBatch) as tf.Tensor3D;
-    const activationProbs =
-        this.activationModel.predict(melSpecBatch) as tf.Tensor3D;
-    const frameProbs =
-        this.frameModel.predict([onsetsProbs, activationProbs]) as tf.Tensor3D;
-  }
-
-  private getAcousticModel() {
+  private getAcousticModel(finalActivation?: string) {
     const acousticModel = tf.sequential();
     const convConfig: any = {
       filters: 32,
@@ -118,6 +190,9 @@ export class OnsetsAndFrames {
     acousticModel.add(
         tf.layers.bidirectional({layer: rnn, mergeMode: 'concat'}));
 
+    acousticModel.add(
+        tf.layers.dense({units: MIDI_PITCHES, activation: finalActivation}));
+
     return acousticModel;
   }
 
@@ -144,6 +219,7 @@ export class OnsetsAndFrames {
     let onsetVelocities = new Uint8Array(MIDI_PITCHES);
     let frame: Uint8Array;
     let onsets: Uint8Array;
+    let previousOnsets = new Uint8Array(MIDI_PITCHES);
 
     function unscaleVelocity(velocity: number) {
       // Translates a velocity estimate to a MIDI velocity value
@@ -186,10 +262,9 @@ export class OnsetsAndFrames {
     const splitFrames = tf.unstack(framePredictions);
     const splitOnsets = tf.unstack(onsetPredictions);
     const splitVelocities = tf.unstack(velocityValues);
-    let previousOnsets = new Uint8Array(onsetPredictions.shape[1]);
-    for (let f = 0; f < splitFrames.length; ++i) {
-      const frame = await splitFrames[f].data() as Uint8Array;
-      const onsets = await splitOnsets[f].data() as Uint8Array;
+    for (let f = 0; f < splitFrames.length; ++f) {
+      frame = await splitFrames[f].data() as Uint8Array;
+      onsets = await splitOnsets[f].data() as Uint8Array;
       const velocities = await splitVelocities[f].data() as Uint8Array;
       for (let p = 0; p < frame.length; ++p) {
         if (frame[p]) {
