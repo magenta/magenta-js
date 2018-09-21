05959c0c96c7f96057cba04826c8f34c4767e71e
Monica Dinculescu <notwaldorf@gmail.com>
2018-09-20 17:04:13 -0700

    export performance stuff (#179)

--- music/src/core/index.ts
+++ music/src/core/index.ts
@@ -20,8 +20,10 @@ import * as chords from './chords';
 import * as constants from './constants';
 import * as data from './data';
 import * as logging from './logging';
+import * as performance from './performance';
 import * as sequences from './sequences';
-export {aux_inputs, chords, constants, data, logging, sequences};
+
+export {aux_inputs, chords, constants, data, logging, performance, sequences};
 
 export * from './midi_io';
 export * from './player';
