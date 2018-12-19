40bafe568df08ddd6e5d9a8c339c094d8f02e918
Adam Roberts <adarob@google.com>
2018-12-18 20:13:15 -0800

    update image docs (#232)

--- docs/image/demos/arbitrary_stylization_bundle.js
+++ docs/image/demos/arbitrary_stylization_bundle.js
@@ -1,40 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var mi = require("../src/index");
 var model = new mi.ArbitraryStyleTransferNetwork();
@@ -52,35 +17,9 @@ function clearCanvas() {
     ctx.fillText("...", canvas.width / 2, canvas.height / 2);
 }
 function stylize() {
-    return __awaiter(this, void 0, void 0, function () {
-        var styleRepresentation, stylized;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4, mi.tf.nextFrame()];
-                case 1:
-                    _a.sent();
-                    clearCanvas();
-                    return [4, mi.tf.nextFrame()];
-                case 2:
-                    _a.sent();
-                    return [4, mi.tf.tidy(function () {
-                            return model.predictStyleParameters(styleImg);
-                        })];
-                case 3:
-                    styleRepresentation = _a.sent();
-                    return [4, mi.tf.tidy(function () {
-                            return model.stylize(contentImg, styleRepresentation);
-                        })];
-                case 4:
-                    stylized = _a.sent();
-                    return [4, mi.tf.toPixels(stylized, canvas)];
-                case 5:
-                    _a.sent();
-                    styleRepresentation.dispose();
-                    stylized.dispose();
-                    return [2];
-            }
-        });
+    clearCanvas();
+    model.stylize(contentImg, styleImg).then(function (imageData) {
+        ctx.putImageData(imageData, 0, 0);
     });
 }
 function setupDemo() {
@@ -44519,7 +44458,7 @@ var ArbitraryStyleTransferNetwork = (function () {
                 .expandDims());
         });
     };
-    ArbitraryStyleTransferNetwork.prototype.stylize = function (content, bottleneck) {
+    ArbitraryStyleTransferNetwork.prototype.produceStylized = function (content, bottleneck) {
         var _this = this;
         return tf.tidy(function () {
             var image = _this.transformNet.predict([
@@ -44530,6 +44469,22 @@ var ArbitraryStyleTransferNetwork = (function () {
                 bottleneck
             ]);
             return image.squeeze();
+        });
+    };
+    ArbitraryStyleTransferNetwork.prototype.stylize = function (content, style, strength) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            var styleRepresentation = _this.predictStyleParameters(style);
+            if (strength !== undefined) {
+                styleRepresentation = styleRepresentation.mul(tf.scalar(strength)).add(_this.predictStyleParameters(content).mul(tf.scalar(1.0 - strength)));
+            }
+            var stylized = _this.produceStylized(content, styleRepresentation);
+            return tf.toPixels(stylized).then(function (bytes) {
+                var imageData = new ImageData(bytes, stylized.shape[1], stylized.shape[0]);
+                styleRepresentation.dispose();
+                stylized.dispose();
+                resolve(imageData);
+            });
         });
     };
     return ArbitraryStyleTransferNetwork;
