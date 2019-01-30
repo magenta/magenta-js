a968ffba05b13e4b0a8799fa4c00eab63164b32c
Monica Dinculescu <noms@google.com>
2019-01-30 14:42:52 -0800

    use a better default for the pixels per unquantized step

--- music/src/core/visualizer.ts
+++ music/src/core/visualizer.ts
@@ -67,18 +67,21 @@ export class Visualizer {
   constructor(
       sequence: INoteSequence, canvas: HTMLCanvasElement,
       config: VisualizerConfig = {}) {
+    this.noteSequence = sequence;
+    this.sequenceIsQuantized = sequences.isQuantizedSequence(this.noteSequence);
+
+    // Quantized sequences appear "longer" because there's usually more
+    // quantized per note (vs seconds), so pick a better default.
+    const defaultPixelsPerTimeStep = this.sequenceIsQuantized ? 8 : 30;
     this.config = {
       noteHeight: config.noteHeight || 6,
       noteSpacing: config.noteSpacing || 1,
-      pixelsPerTimeStep: config.pixelsPerTimeStep || 30,
+      pixelsPerTimeStep: config.pixelsPerTimeStep || defaultPixelsPerTimeStep,
       noteRGB: config.noteRGB || '8, 41, 64',
       activeNoteRGB: config.activeNoteRGB || '240, 84, 119',
       minPitch: config.minPitch,
       maxPitch: config.maxPitch,
     };
-
-    this.noteSequence = sequence;
-    this.sequenceIsQuantized = sequences.isQuantizedSequence(this.noteSequence);
 
     // Initialize the canvas.
     this.ctx = canvas.getContext('2d');
@@ -154,8 +157,8 @@ export class Visualizer {
           `rgba(${isActive ? this.config.activeNoteRGB : this.config.noteRGB},
           ${opacity})`;
       // Round values to the nearest integer to avoid partially filled pixels.
-      this.ctx.fillRect(Math.round(x), Math.round(y), Math.round(w),
-          noteRenderHeight);
+      this.ctx.fillRect(
+          Math.round(x), Math.round(y), Math.round(w), noteRenderHeight);
       if (isActive) {
         activeNotePosition = x;
       }
