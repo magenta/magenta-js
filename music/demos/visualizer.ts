31809585c62c7b8825a78d2d43bc02901f9bec8d
rogerpasky <pascual.dejuan@bbva.com>
2019-03-11 01:34:01 +0100

    StaffSVG Visualizer MVP

--- music/demos/visualizer.ts
+++ music/demos/visualizer.ts
@@ -20,15 +20,19 @@ import {blobToNoteSequence, urlToNoteSequence} from '../src/index';
 
 import {FULL_TWINKLE_UNQUANTIZED} from './common';
 
+// Section 1 *****
+
 const MIDI_URL = './melody.mid';
 
 let canvasVisualizer: mm.PianoRollCanvasVisualizer;
-let svgVisualizer: mm.PianoRollSVGVisualizer;
+let pianoRollSvgVisualizer: mm.PianoRollSVGVisualizer;
+let staffSvgVisualizer: mm.StaffSVGVisualizer;
 
 const player = new mm.Player(false, {
   run: (note: mm.NoteSequence.Note) => {
     canvasVisualizer.redraw(note, true);
-    svgVisualizer.redraw(note, true);
+    pianoRollSvgVisualizer.redraw(note, true);
+    staffSvgVisualizer.redraw(note, true);
   },
   stop: () => {}
 });
@@ -40,8 +44,9 @@ const seqBtn = document.getElementById('seqBtn') as HTMLButtonElement;
 const tempoInput = document.getElementById('tempoInput') as HTMLInputElement;
 const tempoValue = document.getElementById('tempoValue') as HTMLDivElement;
 const fileInput = document.getElementById('fileInput') as HTMLInputElement;
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const svg = document.getElementsByTagName('svg')[0] as SVGSVGElement;
+const canvas = document.getElementById('piano-roll-canvas-viewer') as HTMLCanvasElement;
+const svg = document.querySelector('svg[id="piano-roll-svg-viewer"]') as SVGSVGElement;
+const staff = document.getElementById('staff-svg-viewer') as HTMLDivElement;
 
 // Set up some event listeners
 urlBtn.addEventListener('click', () => fetchMidi(MIDI_URL));
@@ -70,8 +75,13 @@ function initPlayerAndVisualizer(seq: mm.INoteSequence) {
   playBtn.disabled = false;
   playBtn.textContent = 'Loading';
 
-  canvasVisualizer = new mm.PianoRollCanvasVisualizer(seq, canvas);
-  svgVisualizer = new mm.PianoRollSVGVisualizer(seq, svg);
+  let config = {
+    pixelsPerTimeStep: 60,
+  };
+
+  canvasVisualizer = new mm.PianoRollCanvasVisualizer(seq, canvas, config);
+  pianoRollSvgVisualizer = new mm.PianoRollSVGVisualizer(seq, svg, config);
+  staffSvgVisualizer = new mm.StaffSVGVisualizer(seq, staff, config);
 
   const tempo = seq.tempos[0].qpm;
   player.setTempo(tempo);
@@ -91,3 +101,97 @@ function startOrStop() {
     playBtn.textContent = 'Stop';
   }
 }
+
+
+// Section 2 ******
+
+const ADVANCED_MIDI_URL = './anna-magdalena-bach.mid';
+const UNIFORM_TIME_SIZE = 200;
+
+let visualizerRight: mm.StaffSVGVisualizer;
+let visualizerLeft: mm.StaffSVGVisualizer;
+let sequence: mm.NoteSequence;
+
+const player2 = new mm.Player(false, {
+  run: (note: mm.NoteSequence.Note) => {
+    visualizerRight.redraw(note, true);
+    visualizerLeft.redraw(note, true);
+  },
+  stop: () => {}
+});
+
+// UI elements
+const visualization = document.getElementById('visualization') as HTMLInputElement;
+const playBtn2 = document.getElementById('playBtn2') as HTMLButtonElement;
+const tempoInput2 = document.getElementById('tempoInput2') as HTMLInputElement;
+const tempoValue2 = document.getElementById('tempoValue2') as HTMLDivElement;
+const staffRight = document.getElementById('right-hand') as HTMLDivElement;
+const staffLeft = document.getElementById('left-hand') as HTMLDivElement;
+
+let configRight = {
+  noteHeight: 15,
+  pixelsPerTimeStep: UNIFORM_TIME_SIZE,
+  instruments: [0],
+  defaultKey: 7,
+  scrollOnBars: true
+};
+
+let configLeft = {
+  noteHeight: 15,
+  pixelsPerTimeStep: UNIFORM_TIME_SIZE,
+  instruments: [1],
+  defaultKey: 7,
+  scrollOnBars: true
+}
+
+// Set up some event listeners
+visualization.addEventListener('change', changeVisualization);
+playBtn2.addEventListener('click', () => startOrStop2());
+tempoInput2.addEventListener('input', () => {
+  player2.setTempo(parseInt(tempoInput.value, 10));
+  tempoValue2.textContent = tempoInput.value;
+});
+
+function changeVisualization() {
+  configRight.pixelsPerTimeStep = visualization.checked ? 0 : UNIFORM_TIME_SIZE;
+  configLeft.pixelsPerTimeStep = visualization.checked ? 0 : UNIFORM_TIME_SIZE;
+  initPlayerAndVisualizer2(sequence);
+}
+
+function fetchMidi2(url: string) {
+  urlToNoteSequence(url).then(
+    (seq) => {
+      sequence = seq;
+      initPlayerAndVisualizer2(sequence);
+    }
+  );
+}
+
+function initPlayerAndVisualizer2(seq: mm.INoteSequence) {
+  // Disable the UI
+  playBtn2.disabled = false;
+  playBtn2.textContent = 'Loading';
+
+  visualizerRight = new mm.StaffSVGVisualizer(seq, staffRight, configRight);
+  visualizerLeft = new mm.StaffSVGVisualizer(seq, staffLeft, configLeft);
+
+  const tempo = seq.tempos[0].qpm;
+  player2.setTempo(tempo);
+  tempoValue2.textContent = tempoInput2.value = '' + tempo;
+
+  // Enable the UI
+  playBtn2.disabled = false;
+  playBtn2.textContent = 'Play';
+}
+
+function startOrStop2() {
+  if (player2.isPlaying()) {
+    player2.stop();
+    playBtn2.textContent = 'Play';
+  } else {
+    player2.start(visualizerLeft.noteSequence);
+    playBtn2.textContent = 'Stop';
+  }
+}
+
+fetchMidi2(ADVANCED_MIDI_URL);
