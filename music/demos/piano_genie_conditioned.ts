f5ff477595b1c7cb106ab67e9cb4272de90a3c21
Chris Donahue <christopherdonahue@gmail.com>
2019-04-19 11:59:42 -0700

    Added Piano Genie demo conditioned on context

--- music/demos/piano_genie_conditioned.ts
+++ music/demos/piano_genie_conditioned.ts
@@ -16,7 +16,7 @@
  */
 
 import * as Tone from 'tone';
-import * as mm from '../src/index';
+//import * as mm from '../src/index';
 import { CHECKPOINTS_DIR } from './common';
 import {
   PianoGenie,
@@ -24,55 +24,55 @@ import {
   PianoGenieKeysig,
   PianoGenieKeysigChordFamily,
   PianoGenieKeysigChord,
-  PitchClass,
-  ChordFamily,
+//  PitchClass,
+//  ChordFamily,
 } from '../src/piano_genie/model';
 
-const GENIE_DIR = `${CHECKPOINTS_DIR}/piano_genie/model`
+const GENIE_DIR = `${CHECKPOINTS_DIR}/piano_genie/model`;
 // tslint:disable:max-line-length
-const GENIE_CHECKPOINTS = {
+const GENIE_CHECKPOINTS: {[key:string]: {[key:string]: string}} = {
   // Models conditioned on performance timing features
   dt_only: {
     // Original Piano Genie trained on an older version of the MAESTRO dataset.
     v1: `${GENIE_DIR}/epiano/stp_iq_auto_contour_dt_166006`,
     // Trained on MAESTRO without timing in encoder.
-    enc0_kp10_110114: `${GENIE_DIR}/maestro/dt_only/enc0_kp10_110114`,
+    enc0_kp10: `${GENIE_DIR}/maestro/dt_only/enc0_kp10_110114`,
     // Trained on MAESTRO with timing in encoder.
-    enc1_kp10_135091: `${GENIE_DIR}/maestro/dt_only/enc1_kp10_135091`,
+    enc1_kp10: `${GENIE_DIR}/maestro/dt_only/enc1_kp10_135091`,
   },
   // Models conditioned on timing and key signature.
   keysig: {
     // Conditioning features *not* in encoder.
-    enc0_kp05_114087: `${GENIE_DIR}/maestro/keysig/enc0_kp05_114087`,
+    enc0_kp05: `${GENIE_DIR}/maestro/keysig/enc0_kp05_114087`,
     // Conditioning features in encoder.
-    enc1_kp05_159962: `${GENIE_DIR}/maestro/keysig/enc1_kp05_159962`,
+    enc1_kp05: `${GENIE_DIR}/maestro/keysig/enc1_kp05_159962`,
   },
   // Models conditioned on timing, chord root, and chord family.
   chord: {
     // Conditioning features not in encoder and optional dropout.
-    enc0_kp05_111680: `${GENIE_DIR}/maestro/chord/enc0_kp05_111680`,
-    enc0_kp05_132241: `${GENIE_DIR}/maestro/chord/enc0_kp05_132241`,
-    enc0_kp10_099645: `${GENIE_DIR}/maestro/chord/enc0_kp10_099645`,
+    enc0_kp05_a: `${GENIE_DIR}/maestro/chord/enc0_kp05_111680`,
+    enc0_kp05_b: `${GENIE_DIR}/maestro/chord/enc0_kp05_132241`,
+    enc0_kp10: `${GENIE_DIR}/maestro/chord/enc0_kp10_099645`,
     // Conditioning features in encoder and optional dropout.
-    enc1_kp05_152860: `${GENIE_DIR}/maestro/chord/enc1_kp05_152860`,
-    enc1_kp05_153966: `${GENIE_DIR}/maestro/chord/enc1_kp05_153966`,
-    enc1_kp10_121447: `${GENIE_DIR}/maestro/chord/enc1_kp10_121447`,
+    enc1_kp05_a: `${GENIE_DIR}/maestro/chord/enc1_kp05_152860`,
+    enc1_kp05_b: `${GENIE_DIR}/maestro/chord/enc1_kp05_153966`,
+    enc1_kp10: `${GENIE_DIR}/maestro/chord/enc1_kp10_121447`,
   },
   // Models conditioned on timing, key signature, and chord family.
   keysig_chordfamily: {
     // Conditioning features *not* in encoder.
-    enc0_kp05_126711: `${GENIE_DIR}/maestro/keysig_chordfamily/enc0_kp05_126711`,
+    enc0_kp05: `${GENIE_DIR}/maestro/keysig_chordfamily/enc0_kp05_126711`,
     // Conditioning features in encoder.
-    enc1_kp05_172469: `${GENIE_DIR}/maestro/keysig_chordfamily/enc0_kp05_126711`,
+    enc1_kp05: `${GENIE_DIR}/maestro/keysig_chordfamily/enc0_kp05_126711`,
   },
   // Models conditioned on timing, key signature, chord root, and chord family.
   keysig_chord: {
     // Conditioning features *not* in encoder.
-    enc0_kp05_192822: `${GENIE_DIR}/maestro/keysig_chord/enc0_kp05_192822`,
+    enc0_kp05: `${GENIE_DIR}/maestro/keysig_chord/enc0_kp05_192822`,
     // Conditioning features in encoder.
-    enc1_kp05_159786: `${GENIE_DIR}/maestro/keysig_chord/enc1_kp05_159786`,
+    enc1_kp05: `${GENIE_DIR}/maestro/keysig_chord/enc1_kp05_159786`,
   },
-}
+};
 // tslint:enable:max-line-length
 
 const NUM_BUTTONS = 8;
@@ -81,9 +81,18 @@ const LOWEST_PIANO_KEY_MIDI_NOTE = 21;
 let genie: PianoGenie;
 let temperature = 0.25;
 
+function resetConditioningSelectors() {
+  const resetEls = document.getElementsByClassName('rs');
+  for (let i = 0; i < resetEls.length; ++i) {
+    (resetEls[i] as HTMLSelectElement).selectedIndex = 0;
+  }
+}
+
 function selectModel(condtype: string, model: string) {
   document.getElementById('loading').style.display = 'block';
   document.getElementById('loaded').style.display = 'none';
+
+  resetConditioningSelectors();
 
   const ckpt = GENIE_CHECKPOINTS[condtype][model];
 
@@ -92,15 +101,15 @@ function selectModel(condtype: string, model: string) {
   }
   genie = undefined;
 
-  if (condtype == 'dt_only') {
+  if (condtype === 'dt_only') {
     genie = new PianoGenie(ckpt);
-  } else if (condtype == 'keysig') {
+  } else if (condtype === 'keysig') {
     genie = new PianoGenieKeysig(ckpt);
-  } else if (condtype == 'chord') {
+  } else if (condtype === 'chord') {
     genie = new PianoGenieChord(ckpt);
-  } else if (condtype == 'keysig_chordfamily') {
+  } else if (condtype === 'keysig_chordfamily') {
     genie = new PianoGenieKeysigChordFamily(ckpt);
-  } else if (condtype == 'keysig_chord') {
+  } else if (condtype === 'keysig_chord') {
     genie = new PianoGenieKeysigChord(ckpt);
   } else {
     throw new Error();
@@ -109,21 +118,80 @@ function selectModel(condtype: string, model: string) {
   genie.initialize().then(() => {
     document.getElementById('loading').style.display = 'none';
     document.getElementById('loaded').style.display = 'block';
-    for (let key in GENIE_CHECKPOINTS) {
-      if (key == condtype) {
-        document.getElementById(key).style.display = 'block';
+
+    for (const c in GENIE_CHECKPOINTS) {
+      if (c === condtype) {
+        document.getElementById(c).style.display = 'block';
       } else {
-        document.getElementById(key).style.display = 'none';
+        document.getElementById(c).style.display = 'none';
       }
     }
   });
 }
 
-function initModelSelector() {
+function initModelControls() {
+  const kk = document.getElementById('keysig_keysig') as HTMLSelectElement;
+  kk.onchange = () => {
+    (genie as PianoGenieKeysig).setKeySignature(Number(kk.value));
+  };
 
+  const ccr = document.getElementById('chord_chordroot') as HTMLSelectElement;
+  const ccf = document.getElementById('chord_chordfamily') as HTMLSelectElement;
+  ccr.onchange = () => {
+    (genie as PianoGenieChord).setChordRoot(Number(ccr.value));
+  };
+  ccf.onchange = () => {
+    (genie as PianoGenieChord).setChordFamily(Number(ccf.value));
+  };
+
+  const kcfk = document.getElementById(
+    'keysig_chordfamily_keysig') as HTMLSelectElement;
+  const kcfcf = document.getElementById(
+    'keysig_chordfamily_chordfamily') as HTMLSelectElement;
+  kcfk.onchange = () => {
+    (genie as PianoGenieKeysigChordFamily).setKeySignature(Number(kcfk.value));
+  };
+  kcfcf.onchange = () => {
+    (genie as PianoGenieKeysigChordFamily).setChordFamily(Number(kcfcf.value));
+  };
+
+  const kck = document.getElementById(
+    'keysig_chord_keysig') as HTMLSelectElement;
+  const kccr = document.getElementById(
+    'keysig_chord_chordroot') as HTMLSelectElement;
+  const kccf = document.getElementById(
+    'keysig_chord_chordfamily') as HTMLSelectElement;
+  kck.onchange = () => {
+    (genie as PianoGenieKeysigChord).setKeySignature(Number(kck.value));
+  };
+  kccr.onchange = () => {
+    (genie as PianoGenieKeysigChord).setChordRoot(Number(kccr.value));
+  };
+  kccf.onchange = () => {
+    (genie as PianoGenieKeysigChord).setChordFamily(Number(kccf.value));
+  };
 }
 
-function initControlsAndAudio() {
+function initModelSelector() {
+  const modelSelector = document.getElementById('models') as HTMLSelectElement;
+  for (const c in GENIE_CHECKPOINTS) {
+    for (const n in GENIE_CHECKPOINTS[c]) {
+      const option = document.createElement('option') as HTMLOptionElement;
+      option.text = c + ' ' + n;
+      if (c === 'dt_only' && n === 'v1') {
+        option.selected = true;
+      }
+      modelSelector.add(option);
+    }
+  }
+  modelSelector.onchange = () => {
+    const cn = modelSelector.options[modelSelector.selectedIndex].text;
+    const [c, n] = cn.split(' ');
+    selectModel(c, n);
+  };
+}
+
+function initGlobalControlsAndAudio() {
   const heldButtonToMidiNote = new Map<number, number>();
   const synth = new Tone.PolySynth(NUM_BUTTONS, Tone.FMSynth).toMaster();
 
@@ -165,8 +233,21 @@ function initControlsAndAudio() {
       }
     }
   };
+
+  const tempSlider = document.getElementById('temp') as HTMLInputElement;
+  tempSlider.oninput = () => {
+    temperature = Number(tempSlider.value) / Number(tempSlider.max);
+  };
+
+  (document.getElementById('reset') as HTMLButtonElement).onclick = () => {
+    if (genie !== undefined) {
+      resetConditioningSelectors();
+      genie.resetState();
+    }
+  };
 }
 
-selectModel('dt_only', 'v1');
+initModelControls();
 initModelSelector();
-initControlsAndAudio();
+selectModel('dt_only', 'v1');
+initGlobalControlsAndAudio();
