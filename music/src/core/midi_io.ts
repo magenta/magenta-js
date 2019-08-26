b10e4c1ce8a357167d4cfeac6315ed451ed7def8
Monica Dinculescu <noms@google.com>
2019-08-26 16:22:11 -0700

    fixes #315

--- music/src/core/midi_io.ts
+++ music/src/core/midi_io.ts
@@ -223,7 +223,12 @@ export function blobToNoteSequence(blob: Blob): Promise<NoteSequence> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.onload = (e) => {
-      resolve(midiToSequenceProto(reader.result as string));
+      try {
+        const ns = midiToSequenceProto(reader.result as string);
+        resolve(ns);
+      } catch (error) {
+        reject(error);
+      }
     };
     reader.onerror = (e) => reject(e);
     reader.readAsBinaryString(blob);
