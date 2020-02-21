c5e8a579b0034e7636063bb1a419f280c57a5b20
Monica Dinculescu <noms@google.com>
2020-02-20 22:48:09 -0800

    omg hide non-exported modules i did it!!

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -35,18 +35,44 @@ baseDir=$(git rev-parse --show-toplevel)
 # Generation variables.
 urlPrefix="https://github.com/${ORG_NAME}/magenta-js/tree/master/${PKG_NAME}/src/"
 keepAfter="/src/"
+scriptToFixTheToc=""
 
 if [ $PKG_NAME == "image" ]
 then
   urlPrefix="$urlPrefix/arbitrary_stylization/"
   keepAfter="/arbitrary_stylization/"
+elif [ $PKG_NAME == "music" ]
+then
+  # The root index.ts file has a bunch of "export * from './foo';" lines.
+  # Parse those lines into a space separated list of names. It's ok that
+  # they're space separated, we'll split them in JS, this is all a horror anyway.
+  exports=`sed -n "s/export \* from '.\/\(.*\)';/\1/p" $currDir/src/index.ts`
+  scriptToFixTheToc="<script> \
+const toc = \"$exports\".split(' '); \
+const links = document.querySelectorAll('.tsd-kind-external-module'); \
+for (let i = 0; i < links.length; i++) { \
+  const name = links[i].textContent.trim().replace(/\"/g, ''); \
+  if (toc.indexOf(name) === -1) { \
+    links[i].parentNode.removeChild(links[i]); \
+  } \
+} \
+</script>"
 fi
 
 # Generate the docs.
 rm -rf $tmpDir
 npx typedoc --options typedoc.json src --out $tmpDir
 
-# This has generated a bunch of 'Defined in <a href="https://github.com/notwaldorf/magenta-js/blob/c48f0b9/music/src/..."''
+###
+# Here begin the different hacks to fix the docs because typedoc has a lot of bugs.
+###
+
+# Typedoc generates documentation for _all_ files, not just the ones
+# actually exported in the library, so insert a script to fix the index.html.
+# see https://github.com/TypeStrong/typedoc/issues/639
+echo $scriptToFixTheToc >> $tmpDir/index.html
+
+# Typedoc has also generated a bunch of 'Defined in <a href="https://github.com/some-user/magenta-js/blob/some-hash/music/src/..."''
 # links that we need to change to 'Defined in <a href="${urlPrefix}/...' links.
 # We used to be using typedoc-plugin-sourcefile-url to do this, but it stopped
 # working at some point and for loops work well enough.
