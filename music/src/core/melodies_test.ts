def36fdd74f1f4e2766b5d960ca96a7f23e57298
Ian Simon <iansimon@google.com>
2020-04-23 16:08:58 -0700

    add rhythm and shape melody control signals

--- music/src/core/melodies_test.ts
+++ music/src/core/melodies_test.ts
@@ -15,9 +15,10 @@
  * limitations under the License.
  */
 
+import * as tf from '@tensorflow/tfjs';
 import * as test from 'tape';
 import {NoteSequence} from '../protobuf/index';
-import {Melody} from './melodies';
+import {Melody, MelodyRhythm, MelodyShape} from './melodies';
 
 const TEST_NS = NoteSequence.create({
   notes: [
@@ -56,5 +57,26 @@ test('Test Melody To NoteSequence', (t: test.Test) => {
   const expected = TEST_NS;
   expected.notes.pop();
   t.deepEqual(ns, expected);
+  t.end();
+});
+
+test('Test Melody Rhythm Control', (t: test.Test) => {
+  const events = new Int32Array([0, 2, 0, 1, 4, 6, 9, 1]);
+  const melody = new Melody(events, 60, 72);
+  const mr = new MelodyRhythm();
+  const rhythmTensor = mr.extract(melody);
+  t.deepEqual(rhythmTensor.shape, [8, 1]);
+  t.deepEqual(rhythmTensor.dataSync(), [0, 1, 0, 0, 1, 1, 1, 0]);
+  t.end();
+});
+
+test('Test Melody Shape Control', (t: test.Test) => {
+  const events = new Int32Array([0, 2, 0, 6, 6, 2, 9, 1]);
+  const melody = new Melody(events, 60, 72);
+  const ms = new MelodyShape();
+  const shapeTensor = ms.extract(melody);
+  const shapeIndices = tf.argMax(shapeTensor, 1);
+  t.deepEqual(shapeTensor.shape, [8, 3]);
+  t.deepEqual(shapeIndices.dataSync(), [2, 2, 2, 2, 1, 0, 2, 2]);
   t.end();
 });
