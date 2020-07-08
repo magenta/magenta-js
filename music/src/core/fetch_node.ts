be084cd2ac83c3eb1c9e7b2c84b9d574457c98c1
Andy Coenen <andycoenen@google.com>
2020-07-08 10:46:37 -0700

    A few quick cleanups

--- music/src/core/fetch_node.ts
+++ music/src/core/fetch_node.ts
@@ -4,7 +4,7 @@
  * fires callbacks for every audible click, quarter and bar marks.
  *
  * @license
- * Copyright 2012 Google Inc. All Rights Reserved.
+ * Copyright 2020 Google Inc. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
